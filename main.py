0b965a8e348978c43fbb9960bc21bd9a28e2fdec
Dhruv <dhruv.nitj@gmail.com>
2024-11-01 18:23:08 +0530

    fixed changes

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from utils import clean_text
 
 def app(llm: Chain, portfolio):
     st.title('Generate Cold Email Using AI')
-    url = st.text_input(label_visibility="visible", placeholder='Paste the link to the job opening here!')
+    url = st.text_input(placeholder='Paste the link to the job opening here!')
     submit_button = st.button("Click Me!")
     if submit_button:
         try:
